2534e2389e82478ca8819d23a65a4f4ede6243b1
codenoob66 <167981239+codenoob66@users.noreply.github.com>
2024-05-14 20:17:26 +0800

    For deployment

--- script.js
+++ script.js
@@ -2,15 +2,47 @@ const apiKey = "ab7c4e0252b0c3c7352919ad71ab38a7";
 const url = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(url + city + `&appid=${apiKey}`);
-  var data = await response.json();
+  const data = await response.json();
 
-  console.log(data);
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/images/clear.png";
+    } else if (data.weather[0].main == "Drizzle.png") {
+      weatherIcon.src = "images/images/drizzle.png";
+    } else if (data.weather[0].main == "Rain.png") {
+      weatherIcon.src = "images/images/rain.png";
+    } else if (data.weather[0].main == "Snow.png") {
+      weatherIcon.src = "images/images/snow.png";
+    } else if (data.weather[0].main == "Mist.png") {
+      weatherIcon.src = "images/images/mist.png";
+    }
+  }
+
   document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °";
+  document.querySelector(".humidity").innerHTML = data.main.humidity;
+  document.querySelector(".wind").innerHTML =
+    Math.round(data.wind.speed) + " km/h";
+
+  document.querySelector(".weather").style.display = "block";
+  document.querySelector(".error").style.display = "none";
 }
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
 });
